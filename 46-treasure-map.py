4438d7ec2e183f9be61580dec8ba0a7e505505e8
Stephen Carroll <carrollt@synopsys.com>
2023-05-03 17:17:33 -0600

    added second solution to 46...

--- 46-treasure-map.py
+++ 46-treasure-map.py
@@ -7,11 +7,18 @@ print(f"{row1}\n{row2}\n{row3}")
 
 position = input("Where do you want to put the treasure? ")
 
+
+# Solution 1
 X = int(position[0])
 Y = int(position[1])
+map[Y-1][X-1] = 'X'
 
+# Solution 2
+#X = int(position[0])
+#Y = int(position[1])
 
-map[X-1][Y-1] = 'X'
+#selected_row = map[Y -1]
+#selected_row[X - 1] = 'X'
 
 
 print(f"{row1}\n{row2}\n{row3}")
