ce53cc217263ed94e1ba5286f2f722b0e408aaa2
Stephen Carroll <carrollt@synopsys.com>
2023-05-04 16:28:23 -0600

    Completed 47-rock-paper-scissors.py

--- 47-rock-paper-scissors.py
+++ 47-rock-paper-scissors.py
@@ -25,4 +25,37 @@ scissors = '''
 ---.__(___)
 '''
 
+import random
+# Rock beats scissors
+# Scissors beats paper
+# Paper beats rock
 
+
+# My solution
+hand = [rock, paper, scissors]
+#p1_throw = random.randint(0,2)
+p1_throw = int(input("What do you choose?\n 0 - Rock\n 1 - Paper\n 2 - Scissors\n: "))
+p2_throw = random.randint(0,2)
+
+print(f"P1: {hand[p1_throw]} \nP2 (CPU): {hand[p2_throw]} ")
+
+if p1_throw == 0 and p2_throw == 0:
+    print("Result: Draw")
+elif p1_throw == 1 and p2_throw == 1:
+    print("Result: Draw")
+elif p1_throw == 2 and p2_throw == 2:
+    print("Result: Draw")
+elif p1_throw == 0 and p2_throw == 1:
+    print("Result: You lose")
+elif p1_throw == 1 and p2_throw == 0:
+    print("Result: You win")
+elif p1_throw == 2 and p2_throw == 1:
+    print("Result: You win")
+elif p1_throw == 0 and p2_throw == 2:
+    print("Result: You win")
+elif p1_throw == 1 and p2_throw == 2:
+    print("Result: You lose")
+elif p1_throw == 2 and p2_throw == 0:
+    print("Result: You lose")
+    
+    
